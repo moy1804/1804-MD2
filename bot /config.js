2629c33e11c8d7afb8b443669614b24f54424905
moy1804 <pascalmoy1804@gmail.com>
2026-06-27 16:37:13 -0400

    Delete bot /config.js

--- bot /config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  botName: "1804-MD",
-  version: "1.0",
-  footerTag: "1804-PPN version 1.0",
-  defaultPrefix: ".",
-  profileImagePath: __dirname + "/../assets/1804_flag.png",
-};
